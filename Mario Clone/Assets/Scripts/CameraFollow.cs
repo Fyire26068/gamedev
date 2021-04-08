6cf6d6dff3e689341b73b683e98e45685bee09dc
Anthony Garrard <anthony.garrard@tooeleschools.org>
2021-04-08 14:17:31 -0600

    added mario clone, just needs background color and question blocks added to scene

---
+++ Mario Clone/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{   
+    public Transform target;
+    public Transform rightbound;
+    public Transform leftbound;
+
+    private float camHeigth, camWidth, levelminX, levelmaxX;
+
+    public float smoothDampTime = 0.15f;
+    private Vector3 smoothDampVelocity = Vector3.zero;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //calculating camera size
+        camHeigth = Camera.main.orthographicSize * 2; 
+        camWidth = camHeigth* Camera.main.aspect;
+
+        //getting width of our bound right and left
+        float leftBoundWidth =  leftbound.GetComponentInChildren<SpriteRenderer>().bounds.size.x / 2;
+        float rightBoundWidth = rightbound.GetComponentInChildren<SpriteRenderer>().bounds.size.x / 2;
+
+        //setting the min and max x cords
+        levelminX = leftbound.position.x + leftBoundWidth + (camWidth / 2);   
+        levelmaxX = rightbound.position.x + rightBoundWidth - (camWidth / 2) -1;      
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // binding the camera to the player and the bounds
+        // this will set the player as the target of the camera
+        // camera until the camera hits a bound
+        // then it will switch the target to the bounds end
+        // stop the camera moving with the player
+        float targetX = Mathf.Max(levelminX, Mathf.Min(levelmaxX, target.position.x));
+
+        float x = Mathf.SmoothDamp(transform.position.x, targetX, ref smoothDampVelocity.x, smoothDampTime);  
+        transform.position = new Vector3(x, transform.position.y, transform.position.z);   
+    }
+}
---
+++ Mario Clone/Assets/Scripts/Player.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public Vector2 velocity;
+
+    public LayerMask wallMask;
+
+    private bool walk, walk_left, walk_right, jump;
+
+    public void checkPlayerInput()
+    {
+        bool input_left = Input.GetKey(KeyCode.LeftArrow);
+        bool input_right = Input.GetKey(KeyCode.RightArrow);
+        bool input_jump = Input.GetKey(KeyCode.Space);
+
+        walk = input_left || input_right;
+        walk_left = input_left && !input_right;
+        walk_right = !input_left && input_right;
+        jump = input_jump;
+    }
+
+    public void updatePlayerPos()
+    {
+        Vector3 pos = transform.localPosition;
+        Vector3 scale = transform.localScale;
+
+        if(walk)
+        {
+            if(walk_left)
+            {
+                pos.x -= velocity.x * Time.deltaTime;
+                scale.x = -1;
+            }
+            if(walk_right)
+            {
+                pos.x += velocity.x * Time.deltaTime;
+                scale.x = 1;
+            }
+            pos = checkWallRays(pos, scale.x)
+        }
+
+        transform.localPosition = pos;
+        transform.localScale = scale;
+        
+
+    }
+
+    public Vector3 checkWallRays(Vector3 pos, float direction)
+    {
+        Vector2 originTop = new Vector2(pos.x + direction * .4f, pos.y + 1f - 0.2f);
+        Vector2 originMid = new Vector2(pos.x + direction * .4f, pos.y);
+        Vector2 originBot = new Vector2(pos.x + direction * .4f, pos.y - 1f + 0.2f);
+
+        RaycastHit2D wallTop = Physics2D.Raycast(originTop, new Vector2(direction, 0), velocity.x*Time.deltaTime, wallMask);
+        RaycastHit2D wallMid = Physics2D.Raycast(originMid, new Vector2(direction, 0), velocity.x*Time.deltaTime, wallMask);
+        RaycastHit2D wallBot = Physics2D.Raycast(originBot, new Vector2(direction, 0), velocity.x*Time.deltaTime, wallMask);
+        
+        if ((wallTop.collider != null || wallBot.collider != null || wallMid.collider != null))
+        {
+            pos.x -= velocity.x * Time.deltaTime * direction;
+
+        }
+        
+        return pos;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        checkPlayerInput();
+        updatePlayerPos();
+    }
+}
